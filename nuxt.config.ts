e5fe8efebd25c21528c3c6c275a4e5ca9f391a59
Bobbie Goede <bobbiegoede@gmail.com>
2024-03-18 17:25:12 +0100

    chore: setup reproduction

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,31 +1,28 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-	modules: ["@nuxtjs/i18n"],
-	i18n: {
-		lazy: true,
-		langDir: "locales",
-		defaultLocale: "en",
-		locales: [
-			{
-				name: "English",
-				code: "en",
-				iso: "en-US",
-				file: "en.json",
-			},
-			{
-				name: "Nederlands",
-				code: "nl",
-				iso: "nl-NL",
-				file: "nl.json",
-			},
-			{
-				name: "日本語",
-				code: "ja",
-				iso: "ja-JA",
-				file: "ja.yaml",
-			},
-		],
-		strategy: "prefix",
-		detectBrowserLanguage: false,
-	},
+  modules: ["@nuxtjs/i18n"],
+  i18n: {
+    lazy: true,
+    langDir: "locales",
+    defaultLocale: "en",
+    experimental: {
+      localeDetector: "./localeDetector.ts",
+    },
+    locales: [
+      {
+        name: "English",
+        code: "en",
+        iso: "en-US",
+        file: "en.json",
+      },
+      {
+        name: "日本語",
+        code: "ja",
+        iso: "ja-JA",
+        file: "ja.yaml",
+      },
+    ],
+    strategy: "prefix",
+    detectBrowserLanguage: false,
+  },
 });
